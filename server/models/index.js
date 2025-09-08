c9565489cf6129110dd248f8e5cdbec16a62f6a2
muhammad shabas <shabasmuhammedshabas@gmail.com>
2025-09-08 23:11:38 +0530

    new changes

--- server/models/index.js
+++ server/models/index.js
@@ -63,63 +63,6 @@
 
 
 
-
-
-
-
-
-
-// // models/index.js
-// import sequelize from "../connect/connect.js";
-// import Shops from "./shopModel.js";
-// import Customer from "./customerModel.js";
-// import Order from "./orderModel.js";
-// import Product from "./ProductModel.js";
-
-// // Initialize models
-// const models = {
-//   Shops,
-//   Customer,
-//   Order,
-//   Product
-//   // Add other models here as needed
-// };
-
-// // Set up associations
-// Object.keys(models).forEach(modelName => {
-//   if (models[modelName].associate) {
-//     models[modelName].associate(models);
-//   }
-// });
-
-// // Test the database connection and sync models
-// (async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Database connection has been established successfully.');
-    
-//     // Sync all models
-//     await sequelize.sync({ alter: true });
-//     console.log('All models were synchronized successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-// })();
-
-// // Export all models + sequelize instance
-// export { 
-//   sequelize, 
-//   Shops, 
-//   Customer, 
-//   Order, 
-//   Product 
-// };
-
-
-
-
-
-
 import sequelize from "../connect/connect.js";
 import Shops from "./shopModel.js";
 import Customer from "./customerModel.js";
